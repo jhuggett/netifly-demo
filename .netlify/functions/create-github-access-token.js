73f4fd5ab05c54fe1ba508ade2393be88db8386a
Joel Huggett <joel@forestry.io>
2020-06-23 09:53:13 -0300

    changed callbacks

--- .netlify/functions/create-github-access-token.js
+++ .netlify/functions/create-github-access-token.js
@@ -11,34 +11,34 @@ const AES = require('crypto-js').AES
 
 
 exports.handler = (clientId, secret, signingKey) => (event, context, callback) => {
-    console.log({ event, context });
 
 
-    createAccessToken(clientId, secret, event.query.code, event.query.state).then(
+    createAccessToken(clientId, secret, event.queryStringParameters.code, event.queryStringParameters.state).then(
         (tokenResp) => {
-        const { access_token, error } = qs.parse(tokenResp.data)
-        if (error) {
-            callback(error)
-        } else {
-            // Generate the csrf token
-            const csrfToken = uuidv4()
-    
-            // Sign the amalgamated token
-            const unsignedToken = `${csrfToken}.${access_token}`
-            const signedToken = AES.encrypt(unsignedToken, signingKey).toString()
-    
-            // Set the csrf token as an httpOnly cookie
-            // res.setHeader(
-            // 'Set-Cookie',
-            // serialize(CSRF_TOKEN_KEY, csrfToken, {
-            //     path: '/',
-            //     httpOnly: true,
-            // })
-            // )
-    
-            // // Return the amalgamated token
-            // res.status(200).json({ signedToken })
-        }
+            const { access_token, error } = qs.parse(tokenResp.data)
+            if (error) {
+                callback(error)
+            } else {
+                // Generate the csrf token
+                const csrfToken = uuidv4()
+        
+                // Sign the amalgamated token
+                const unsignedToken = `${csrfToken}.${access_token}`
+                const signedToken = AES.encrypt(unsignedToken, signingKey).toString()
+        
+                // Set the csrf token as an httpOnly cookie
+                // res.setHeader(
+                // 'Set-Cookie',
+                // serialize(CSRF_TOKEN_KEY, csrfToken, {
+                //     path: '/',
+                //     httpOnly: true,
+                // })
+                // )
+
+
+                // Return the amalgamated token
+                callback(null, 200, { signedToken })
+            }
         }
     )
 }
