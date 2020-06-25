01f20652267eb5cd3443e2097353e552233ad926
Joel Huggett <joel@forestry.io>
2020-06-25 15:11:57 -0300

    set cookie

--- functions/create-github-access-token.js
+++ functions/create-github-access-token.js
@@ -10,8 +10,6 @@ const AES = require('crypto-js').AES
 
 
 const route = (clientId, secret, signingKey) => async (event, context, callback) => {
-    console.log("CALLED");
-    
     const tokenResp = await createAccessToken(clientId, secret, event.queryStringParameters.code, event.queryStringParameters.state)
     const { access_token, error } = qs.parse(tokenResp.data)
     if (error) {
@@ -25,19 +23,18 @@ const route = (clientId, secret, signingKey) => async (event, context, callback)
         const signedToken = AES.encrypt(unsignedToken, signingKey).toString()
 
         // Set the csrf token as an httpOnly cookie
-        // res.setHeader(
-        // 'Set-Cookie',
-        // serialize(CSRF_TOKEN_KEY, csrfToken, {
-        //     path: '/',
-        //     httpOnly: true,
-        // })
-        // )
-
+        const csrfCookie = serialize('csrfToken', csrfToken, {
+            path: '/',
+            httpOnly: true
+        })
 
         // Return the amalgamated token
         callback(null, {
             statusCode: 200,
-            body: "HELLO WORLD"
+            headers: {
+                'Set-Cookie': csrfCookie
+            },
+            body: JSON.stringify({ signedToken: signedToken })
         })
 
     }
